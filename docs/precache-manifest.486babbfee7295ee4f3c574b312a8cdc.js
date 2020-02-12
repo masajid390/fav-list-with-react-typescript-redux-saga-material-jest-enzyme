2b21c5333e270fb5f5c9cf0ef3e8311f7c38da94
MUHAMMAD ARSLAN SAJID <muhammadarslansajid@MUHAMMADs-MacBook-Pro.local>
2020-02-12 05:11:31 +0500

    added homepage to package.json to fix github pages demo

---
+++ docs/precache-manifest.486babbfee7295ee4f3c574b312a8cdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfab5e451ba5ae87bb3fa2a6452529c3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0342042da45c22389d1b",
+    "url": "./static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "aa442fa429daf724c6ae",
+    "url": "./static/js/2.9ee14e4e.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "./static/js/2.9ee14e4e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0342042da45c22389d1b",
+    "url": "./static/js/main.5ddac4a1.chunk.js"
+  },
+  {
+    "revision": "8c14e8f0b0435b146618",
+    "url": "./static/js/runtime-main.60d34108.js"
+  }
+]);
